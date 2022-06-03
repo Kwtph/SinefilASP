1be45c40ac1936c077f453f4e0b0ad6eac8be7e3
K.w.t.p.h <92263264+ogulcanaltun@users.noreply.github.com>
2022-06-04 00:56:59 +0300

    v2.1

--- SinemaAppWeb/Controllers/MovieController.cs
+++ SinemaAppWeb/Controllers/MovieController.cs
@@ -21,5 +21,6 @@ namespace SinemaAppWeb.Controllers
         {
             return View();
         }
+
     }
 }
--- SinemaAppWeb/Data/Repository.cs
+++ SinemaAppWeb/Data/Repository.cs
@@ -9,12 +9,12 @@ namespace SinemaAppWeb.Data
 {
     public class MovieRepository
     {
-        private static List<Movie> _movies = null; //_movies isimli boş bir liste oluşturduk.
+        public static List<Movie> _movies = null; //_movies isimli boş bir liste oluşturduk.
         static MovieRepository()
         {
             _movies = new List<Movie>()
             {
-                new Movie(){ Id=1, Name="Titanic", Description="A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", Yonetmen= "James Cameron", Oyuncular="Leonardo DiCaprio, Kate Winslet, Billy Zane", ImageUrl="https://tr.web.img4.acsta.net/r_1280_720/medias/nmedia/18/89/86/41/20070382.jpg", TrailerUrl="https://www.youtube.com/embed/kVrqfYjkTdQ", CategoryId=1,IMDbStar="tt0120338"},
+                new Movie(){ Id=1, Name="Titanic", Description="A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", Yonetmen= "James Cameron", Oyuncular="Leonardo DiCaprio, Kate Winslet, Billy Zane", ImageUrl="https://tr.web.img4.acsta.net/r_1280_720/medias/nmedia/18/89/86/41/20070382.jpg", TrailerUrl="https://www.youtube.com/embed/kVrqfYjkTdQ", CategoryId=1,IMDbStar=""},
                 new Movie(){ Id=2, Name="", Description="", Yonetmen= "", Oyuncular="", ImageUrl="", CategoryId=1},
                 new Movie(){ Id=3, Name="", Description="", Yonetmen= "", Oyuncular="", ImageUrl="", CategoryId=2},
                 new Movie(){ Id=4, Name="", Description="", Yonetmen= "", Oyuncular="", ImageUrl="", CategoryId=2},
